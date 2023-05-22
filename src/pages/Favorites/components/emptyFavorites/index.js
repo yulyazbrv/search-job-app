da6135f77451679411b321ee92a1d63c0d86ba57
yulyazbrv <zubareva.yo@gmail.com>
2023-05-22 19:09:11 +0300

    feat: add click on button search

--- src/pages/Favorites/components/emptyFavorites/index.js
+++ src/pages/Favorites/components/emptyFavorites/index.js
@@ -1,13 +1,20 @@
 import { Button, Flex } from '@mantine/core';
 import emptyIcon from './assets/empty.png';
+import { useNavigate } from 'react-router-dom';
 import './index.css';
 
 const EmptyFavorites = () => {
+  const navigate = useNavigate();
+
+  const backToSearch = () => {
+    navigate("/");
+  };
   return (
     <Flex direction={'column'} gap={32} align={'center'} mt={104}>
       <img src={emptyIcon} alt="emptyIcon" className="empty-img"></img>
       <p className="empty-title">Упс, здесь ничего нет!</p>
       <Button
+      onClick={backToSearch}
         radius={8}
         styles={{
           root: {
